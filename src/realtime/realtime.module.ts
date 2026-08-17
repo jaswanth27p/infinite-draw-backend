f0fee500c33f79b6f6e5f30f0c325b79697f8da9
jaswanth27p <jaswanth27p@gmail.com>
2026-08-17 20:12:57 +0530

    feat: register CollabGateway and WsLocalUserGuard in RealtimeModule

--- src/realtime/realtime.module.ts
+++ src/realtime/realtime.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { FilesModule } from '../files/files.module';
 import { PingGateway } from './ping.gateway';
 import { WsClerkGuard } from './ws-clerk.guard';
+import { WsLocalUserGuard } from './ws-local-user.guard';
+import { CollabGateway } from './collab.gateway';
 
 @Module({
-  providers: [PingGateway, WsClerkGuard],
+  imports: [FilesModule],
+  providers: [PingGateway, WsClerkGuard, WsLocalUserGuard, CollabGateway],
   exports: [WsClerkGuard],
 })
 export class RealtimeModule {}
